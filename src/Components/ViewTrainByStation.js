a3670381980f2f75bace4b54427e2eaf1560f9ff
zeelib <zvonimirdamjanovic@gmail.comgit config --global user.email zvonimirdamjanovic@gmail.com>
2020-09-29 18:06:22 +0200

    Train by station React table

--- src/Components/ViewTrainByStation.js
+++ src/Components/ViewTrainByStation.js
@@ -1,15 +1,52 @@
   
 import React, { useEffect, useState } from "react";
 import './../Styles/ViewTrainByStation.css';
+import axios from 'axios';
 
 const ViewTrainByStation = (props) => {
-    const {stationid} = props.match.params;
-    const {history} = props;
-    /* const [data, setData] = useState([]);
-    const [error, setError] = useState(""); */
-  
+    // const {stationid} = props.match.params;
+    const [station, setStation] = useState({});
+
+   useEffect(() => {
+    axios.get('http://localhost:3000/stations').then(data => setStation(data)).catch(err=>console.log(err));
+
+   }, []);
+  console.log(station);
     return (
-        <div className="railway-station">View Template</div>
+        <div className="railway-station">
+            <h1>Train by Station</h1>
+   
+            {station.data ? station.data.map((element,index) => {
+                return(
+                    <div key={index}>
+                        
+                <table className="railway-station-table">
+                    <thead className="railway-station-table-head">
+                    <tr className="railway-station-table-column-head">
+                        <th>Name</th>
+                        <th>City</th>
+                        <th>Company</th>
+                        <th>Country</th>
+                        <th>Length</th>
+                        <th>Maintenance/Condition</th>
+                    </tr>
+                    </thead>
+
+                    <tbody className="railway-station-table-body">
+                    <tr className= "railway-station-table-column-head">
+                        <td>{element.name}</td>
+                        <td>{element.city}</td>
+                        <td>{element.company}</td>
+                        <td>{element.country}</td>
+                        <td>{element.length}</td>
+                        {element.maintenance === true ?<td>Good</td> : <td>Repair required</td> }
+                    </tr>
+                    </tbody>
+                </table>
+               </div> )
+            }):null}
+
+        </div>
     );
   };
   export default ViewTrainByStation;
