0b8611a61e8e2eccdbde3341021398be5d32dc2c
zeelib <zvonimirdamjanovic@gmail.comgit config --global user.email zvonimirdamjanovic@gmail.com>
2020-10-05 14:34:02 +0200

    Savings

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-require('dotenv').config();
-
-  {/* <React.StrictMode>
-    <App />
-  </React.StrictMode>, */}
 
 ReactDOM.render(
   <App />,
